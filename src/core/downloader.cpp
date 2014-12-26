843338f6cfdc785aa908cee38d3b66050b2c861f
Fs <Fsu0413@QSanguosha.org>
2014-12-26 08:02:03 +0800

    untested redirection(server CHOUFENGed again!!!!)

--- src/core/downloader.cpp
+++ src/core/downloader.cpp
@@ -80,40 +80,55 @@ void Downloader::singleFileError(QNetworkReply::NetworkError /*e*/) {
 }
 
 void Downloader::singleFileFinished() {
-    if (m_failedList.contains(m_currentDownloadingFile))
+    if (m_failedList.contains(m_currentDownloadingFile)) {
         emit one_failed(m_currentDownloadingFile);
-    else {
-        QIODevice *r = qobject_cast<QIODevice *>(sender());
-        QString filename = QUrl(m_currentDownloadingFile).fileName();
-        QFile file(m_downloadDir.absoluteFilePath(filename));
-        file.open(QIODevice::Truncate | QIODevice::WriteOnly);
-        file.write(r->readAll());
-        file.close();
+        downloadSingleFile();
+    } else {
+        QNetworkReply *r = qobject_cast<QNetworkReply *>(sender());
 
-        if (filename.endsWith(".jpg")) { // important hack!!
-            QString new_filename = filename;
-            new_filename.chop(4);
-            new_filename.append(".png");
-            //m_downloadDir.rename(filename, new_filename);
-            QPixmap pm;
-            if (pm.load(m_downloadDir.absoluteFilePath(filename))) {
-                if (pm.save(m_downloadDir.absoluteFilePath(new_filename), "PNG")) {
-                    m_downloadDir.remove(filename);
+        if (r->attribute(QNetworkRequest::RedirectionTargetAttribute).isNull()) {
+            QString filename = QUrl(m_currentDownloadingFile).fileName();
+            QFile file(m_downloadDir.absoluteFilePath(filename));
+            file.open(QIODevice::Truncate | QIODevice::WriteOnly);
+            file.write(r->readAll());
+            file.close();
+
+            if (filename.endsWith(".jpg")) { // important hack!!
+                QString new_filename = filename;
+                new_filename.chop(4);
+                new_filename.append(".png");
+                //m_downloadDir.rename(filename, new_filename);
+                QPixmap pm;
+                if (pm.load(m_downloadDir.absoluteFilePath(filename))) {
+                    if (pm.save(m_downloadDir.absoluteFilePath(new_filename), "PNG")) {
+                        m_downloadDir.remove(filename);
+                    } else
+                        qDebug() << "save png error " << new_filename;
+                } else if (pm.load(m_downloadDir.absoluteFilePath(filename), "PNG")) {
+                    if (pm.save(m_downloadDir.absoluteFilePath(new_filename), "PNG")) {
+                        m_downloadDir.remove(filename);
+                    } else
+                        qDebug() << "save png error " << new_filename;
                 } else
-                    qDebug() << "save png error " << new_filename;
-            } else if (pm.load(m_downloadDir.absoluteFilePath(filename), "PNG")) {
-                if (pm.save(m_downloadDir.absoluteFilePath(new_filename), "PNG")) {
-                    m_downloadDir.remove(filename);
-                } else
-                    qDebug() << "save png error " << new_filename;
-            } else
-                qDebug() << "load jpg error " << filename;
+                    qDebug() << "load jpg error " << filename;
+            }
+
+            emit one_completed(m_currentDownloadingFile);
+            downloadSingleFile();
+        } else {
+            if (m_cancelRequested) {
+                emit canceled();
+                quit();
+                return;
+            }
+            QUrl u = r->attribute(QNetworkRequest::RedirectionTargetAttribute).toUrl().resolved(QUrl(m_currentDownloadingFile));
+            qDebug() << "redirect!!";
+            qDebug() << u;
+            QNetworkReply *r = mgr.get(QNetworkRequest(u));
+            connect(r, ((void (QNetworkReply::*)(QNetworkReply::NetworkError))(&QNetworkReply::error)), this, &Downloader::singleFileError);
+            connect(r, &QNetworkReply::finished, this, &Downloader::singleFileFinished);
         }
-
-        emit one_completed(m_currentDownloadingFile);
     }
-
-    downloadSingleFile();
 }
 
 Downloader *operator <<(Downloader *downloader, const QString &filename) {
