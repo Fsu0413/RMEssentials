d19e560022292a1425b28d3378ba540bf4bdc71c
Fs <Fsu0413@users.noreply.github.com>
2016-12-24 21:41:07 +0800

    fix path on osx

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -14,6 +14,12 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+#ifdef Q_OS_OSX
+#ifdef QT_NO_DEBUG
+    QDir::setCurrent(qApp->applicationDirPath());
+#endif
+#endif
+
     QTranslator qt_translator;
     qt_translator.load(QStringLiteral(CURRENTDIRPREFIX "qt_zh_CN.qm"));
     qApp->installTranslator(&qt_translator);
