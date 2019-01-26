824f6e95ee60f6a23ae54d83e8b12818184b2f5d
Fs <fs@FsdeMacBook-Air.local>
2019-01-26 14:04:23 +0800

    fix AutoGenerateHeader shell script

--- tools/AutoGenerateHeader.sh
+++ tools/AutoGenerateHeader.sh
@@ -14,7 +14,7 @@ processSingleFile() {
         echo "Copying file $1"
         cp -f $1 $outdir/
     fi
-    classes=`cat $1 | sed -n -E "s/^(class|struct) +[A-Z]*_EXPORT +([A-Za-z0-9_]+)( .*)?/\2/p"`
+    classes=`cat $1 | sed -n -E "s/^(class|struct) +[A-Z]*_EXPORT +([A-Za-z0-9_]+)([^A-Za-z0-9_].*)?/\2/p"`
     for x in $classes; do
         echo "Generating Header for $x"
         echo "#include \"`basename $1`\"" > $outdir/$x
